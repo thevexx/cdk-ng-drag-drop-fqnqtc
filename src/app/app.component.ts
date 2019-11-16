e1eb5af3e6e1e3934ea1ff474fd7ea9683e92d46
thevexx <abe.crunck@gmail.com>
2019-11-16 15:23:18 +0100

    6 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,72 @@
-
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from "@angular/core";
 import {
   CdkDragDrop,
   moveItemInArray,
   CdkDrag,
   CdkDrop,
   transferArrayItem
-} from '@angular/cdk/drag-drop';
+} from "@angular/cdk/drag-drop";
+declare const $: any;
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  items = ['Zero', 'One', 'Two', 'Three'];
+export class AppComponent implements OnInit {
+  items = ["Zero", "One", "Two", "Three"];
 
-  items2 = ['Zero', 'One', 'Two', 'Three'];
-  items3 = [];
+  items2 = [
+    {
+      id: "1",
+      text: "One",
+      link: "0"
+    },
+    {
+      id: "2",
+      text: "Two"
+    },
+    {
+      id: "3",
+      text: "Three"
+    }
+  ];
+  items3 = [
+    {
+      id: "5",
+      text: "Five",
+     },
+    {
+      id: "6",
+      text: "Six",
+     },{
+      id: "4",
+      text: "Zero",
+      link: "0"
+    }
+  ];
+  listItems = [this.items2, this.items3];
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef) {}
 
+  ngOnInit() {
+    let connections;
+    setTimeout(() => {
+      $("#1").connections({
+        to: "#4",
+        borderClasses: {
+          top: "border-top",
+          right: "border-right",
+          bottom: "border-bottom",
+          left: "border-left"
+        }
+      });
+      connections = $("connection, inner");
+    }, 1000);
+    setInterval(() => {
+      connections.connections("update");
+    }, 100);
+  }
   onDrop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.items, event.previousIndex, event.currentIndex);
   }
@@ -29,7 +75,7 @@ export class AppComponent {
     return false;
   };
 
-  addToList(event: CdkDragDrop<string[]>) {
+  addToList(event: CdkDragDrop<any[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
